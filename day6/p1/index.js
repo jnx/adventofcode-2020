f179ee17849cb5246899b1fed67ba47043a2bd2c
jnx <christian.hjalmarsson@svt.se>
2020-12-07 08:39:52 +0000

    Day 6 done

--- day6/p1/index.js
+++ day6/p1/index.js
@@ -1,12 +1,10 @@
 let fs = require('fs-extra')
-let data = fs.readFileSync('../data.txt').toString().split('\n\n')
+let groups = fs.readFileSync('../data.txt').toString().split('\n\n')
 let sum = 0
 
-data.forEach(row => {
-  row = row.replace(/\n/gi, '')
-  let store = new Map()
-  row.split('').forEach(char => store.set(char))
-  sum += store.size
-})
+for (let group of groups) {
+  let uniq = new Set([...group.replace(/\n/gi, '')])
+  sum += uniq.size
+}
 
 console.log(sum)
--- day6/p2/index.js
+++ day6/p2/index.js
@@ -1,12 +1,10 @@
 let fs = require('fs-extra')
-let data = fs.readFileSync('../data.txt').toString().split('\n\n')
+let groups = fs.readFileSync('../data.txt').toString().split('\n\n')
 let sum = 0
 
-data.forEach(row => {
-  row = row.replace(/\n/gi, '')
-  let store = new Map()
-  row.split('').forEach(char => store.set(char))
-  sum += store.size
-})
+for (let group of groups) {
+  let uniq = new Set([...group.replace(/\n/gi, '')])
+  sum += [...uniq].filter(char =>  group.split('\n').every(form => form.includes(char))).length
+}
 
 console.log(sum)
