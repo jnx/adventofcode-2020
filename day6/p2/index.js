637ef8fa5b4e618b02240d93287fd27cdd0e5659
Christian Hjalmarsson <christian.hjalmarsson@svt.se>
2020-12-08 07:28:38 +0100

    Remove a space

--- day6/p2/index.js
+++ day6/p2/index.js
@@ -4,7 +4,7 @@ let sum = 0
 
 for (let group of groups) {
   let uniq = new Set([...group.replace(/\n/gi, '')])
-  sum += [...uniq].filter(char =>  group.split('\n').every(form => form.includes(char))).length
+  sum += [...uniq].filter(char => group.split('\n').every(form => form.includes(char))).length
 }
 
 console.log(sum)
